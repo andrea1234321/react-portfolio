93899a374f14ab81e7ce24b89ae322f52a0ba16c
andrea <andrearewards@hotmail.com>
2025-04-18 11:26:04 -0400

    add icons to work experience and profile card, adjust css to match design vision

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+// import styles from './AboutMe.module.css'
 
 export default function AboutMe() {
   return (
-    <div>AboutMe</div>
-  )
+    <div className="container">
+      <h1>Hi, I'm Andrea, </h1>
+      <h2>a Full-Stack Software Engineer</h2>
+      <h2>with a primary focus in Front-End development</h2>
+    </div>
+  );
 }
--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -1,55 +1,58 @@
-import React from 'react';
-import { 
+import React from "react";
+import {
   Timeline,
   TimelineBody,
   TimelineContent,
   TimelineItem,
   TimelinePoint,
   TimelineTime,
-  TimelineTitle, } from "flowbite-react";
-
+  TimelineTitle,
+} from "flowbite-react";
+import { HiCalendar } from "react-icons/hi";
 
 export default function Work() {
   const workExperience = [
     {
       date: "June 2024 - Present",
-      title: 'Full-Stack Software Engineer',
-      company: 'Deloitte',
-      description: ''
+      title: "Full-Stack Software Engineer",
+      company: "Deloitte",
+      description: "lorem",
     },
     {
       date: "March 2024 - Present",
-      title: 'Java Developer Apprentice',
-      company: 'Skillstorm',
-      description: ''
+      title: "Java Developer Apprentice",
+      company: "Skillstorm",
+      description: "lorem",
     },
     {
       date: "July 2023 - September 2023",
-      title: 'Web Developer',
-      company: 'Freelance',
-      description: ''
+      title: "Web Developer",
+      company: "Freelance",
+      description: "lorem",
     },
     {
       date: "March 2023 - June 2023",
-      title: 'Software Engineering Fellow',
-      company: 'General Assembly',
-      description: ''
-    }
-  ]
+      title: "Software Engineering Fellow",
+      company: "General Assembly",
+      description: "lorem",
+    },
+  ];
   return (
-    <>
+    <div className="container">
       <Timeline>
-        {workExperience.map(experience => (
-            <TimelineItem>
-              <TimelinePoint />
-              <TimelineContent>
-                <TimelineTime>{experience.date}</TimelineTime>
-                <TimelineTitle>{experience.title}</TimelineTitle>
-                <TimelineBody>{experience.description}</TimelineBody>
-              </TimelineContent>
-            </TimelineItem>
+        {workExperience.map((experience) => (
+          <TimelineItem>
+            <TimelinePoint icon={HiCalendar} />
+            <TimelineContent>
+              <TimelineTime>{experience.date}</TimelineTime>
+              <br />
+              <TimelineTime>{experience.company}</TimelineTime>
+              <TimelineTitle>{experience.title}</TimelineTitle>
+              <TimelineBody>{experience.description}</TimelineBody>
+            </TimelineContent>
+          </TimelineItem>
         ))}
       </Timeline>
-    </>
-  )
+    </div>
+  );
 }
