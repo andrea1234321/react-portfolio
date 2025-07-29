41e14060e5eaf7a327b0967deaf0f6522159be33
andrea <andrearewards@hotmail.com>
2025-07-29 14:00:09 -0400

    change font-size for about me section

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import styles from './AboutMe.module.css'
+import styles from './AboutMe.module.css'
 
 export default function AboutMe() {
   return (
-    <div className="container">
+    <div className={`container ${styles.aboutMeContainer}`}>
       <h1>Hi, I'm Andrea, </h1>
       <h2>a Full-Stack Software Engineer</h2>
-      <h2>with a primary focus in Front-End development</h2>
+      <h2>Specializing in Front-End development</h2>
     </div>
   );
 }
