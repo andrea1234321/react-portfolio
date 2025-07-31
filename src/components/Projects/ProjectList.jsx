7d5efe3e69c60e34d8e48a494481e94dd558b470
andrea <andrearewards@hotmail.com>
2025-07-31 13:19:46 -0400

    create project lists with existing projects, add badges to projects, create initial layout for projects

---
+++ src/components/Projects/ProjectList.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { Button } from "flowbite-react";
+import styles from "./ProjectList.module.css";
+
+export default function ProjectList() {
+  const projects = [
+    {
+      name: "Midnight Inventory Management",
+      description:
+        "Independently built an inventory management app to deepen expertise in Java and Spring Boot. The app enables efficient tracking and editing of inventory across multiple warehouses through a user-friendly interface and robust backend, simulating real-world operational needs.",
+      frontEnd: ["JavaScript", "HTML", "CSS"],
+      backEnd: ["Java", "Spring", "PostgreSQL"],
+      githubLink:
+        "https://github.com/andrea1234321/inventory-management-front-end",
+    },
+    {
+      name: "Express Tax",
+      description:
+        "Developed a full-stack tax filing application to streamline the management of personal tax information. Included functionality to create, view, update, and delete tax records, with accessibility settings that allow users to switch between English and Spanish, enhancing usability for a diverse audience.",
+      frontEnd: ["TypeScript", "HTML", "CSS"],
+      backEnd: ["Java", "Spring", "PostgreSQL"],
+      githubLink: "https://github.com/andrea1234321/tax-prep-app-frontend",
+    },
+    {
+      name: "HealthPal",
+      description:
+        'Built a collaborative React application for the "Women Who Code Hackathon for Social Good," leveraging the OpenAI API to deliver personalized medical recommendations based on user demographics, medical history, and current concerns. his project won the Best Web Development category.',
+      frontEnd: ["JavaScript", "HTML", "CSS"],
+      backEnd: ["React", "MongoDb", "Express", "Node", "OpenAI"],
+      githubLink: "https://github.com/andrea1234321/healthPal-front-end",
+      webLink: "https://health-pal.netlify.app/",
+    },
+    {
+      name: "BAMM Voyage",
+      description:
+        "Collaborated with three engineers to build a MERN stack travel planning application designed to simplify trip organization by helping users plan, manage, and track their travel itineraries in one place. The app offers an intuitive interface and a reliable backend to streamline all aspects of travel preparation.",
+      frontEnd: ["JavaScript", "HTML", "CSS"],
+      backEnd: ["React", "MongoDb", "Express", "Node"],
+      sgithubLink: "https://github.com/CurrentlyBob/bamm-voyage-front-end",
+      webLink: "https://bamm-voyage.netlify.app/",
+    },
+    {
+      name: "LevelUp",
+      description: `Independently developed and designed a MEN stack application to help users organize their lives by creating detailed goals and tracking progress. The app also supports identifying and breaking bad habits, offering a comprehensive tool for personal growth and productivity.`,
+      frontEnd: ["JavaScript", "HTML", "CSS"],
+      backEnd: ["MongoDb", "Express", "Node"],
+      githubLink: "https://github.com/andrea1234321/healthPal-front-end",
+    },
+    {
+      name: "BlackJack",
+      description: `Developed a vintage Vegas-themed Blackjack as an initial project to establish foundational JavaScript proficiency. The game incorporates essential Blackjack mechanics including doubling down and accurate ace value logic, delivering an engaging and authentic casino experience.`,
+      frontEnd: ["JavaScript", "HTML", "CSS"],
+      githubLink: "https://github.com/andrea1234321/BlackJack",
+      webLink: "https://andrea-blackjack.netlify.app/",
+    },
+    {
+      name: "Showcase",
+      description: `Developed an application to manage a users favorite shows as a dedicated effort to build proficiency in Python and Django. The project features full CRUD functionalities and demonstrates practical experience in building web applications with these technologies.`,
+      frontEnd: ["Python", "HTML", "CSS"],
+      backEnd: ["PostgreSQL", "Django"],
+      githubLink: "https://github.com/andrea1234321/showcase",
+    },
+  ];
+
+  const techBadges = {
+    JavaScript: "https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E",
+    HTML: "https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white",
+    CSS: "https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white",
+    TypeScript: "https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white",
+    React: "https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB",
+    MongoDb: "https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white",
+    Express: "https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB",
+    Node: "https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white",
+    Python: "https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54",
+    Django: "https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white",
+    PostgreSQL: "https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white",
+    Spring: "https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white",
+    Java: "https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white",
+    OpenAi: 'https://img.shields.io/badge/chatGPT-74aa9c?style=for-the-badge&logo=openai&logoColor=white'
+  };
+
+  return (
+    <>
+      {projects.map((project) => (
+        <div className={styles.projectCard} key={project.name}>
+          <div className={styles.picture}>picture</div>
+          <div className={styles.description}>
+            <h1>{project.name}</h1>
+            <div className={styles.badges}>
+              {project.frontEnd.map((tech, index) => (
+                <img src={techBadges[tech]} alt={`${tech} badge`} key={`frontend-${index}`}></img>
+              ))}
+              {project.backEnd &&
+                project.backEnd.map((tech, index) => (
+                  <img src={techBadges[tech]} alt={`${tech} badge`} key={`frontend-${index}`}></img>
+                ))}
+            </div>
+            <h2>{project.description}</h2>
+            <div className={styles.links}>
+              <Button href={project.githubLink} target="_blank">
+                GitHub
+              </Button>
+              {project.webLink && (
+                <Button href={project.webLink} target="_blank">
+                  Website
+                </Button>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
