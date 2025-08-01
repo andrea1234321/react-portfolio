20738f0638da005307bce810fc1542a31c9673b1
andrea <andrearewards@hotmail.com>
2025-08-01 14:26:22 -0400

    add titles to work and projects section, add certifications to work timeline

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ProjectList from '../../components/Projects/ProjectList'
-import styles from './Projects.module.css'
+import React from "react";
+import ProjectList from "../../components/Projects/ProjectList";
+import styles from "./Projects.module.css";
 
 export default function Projects() {
   return (
-    <div className={styles.projectListContainer}>
-      <ProjectList/>
-    </div>
-  )
+    <>
+      <h1 className={styles.title}>Projects</h1>
+      <div className={styles.projectListContainer}>
+        <ProjectList />
+      </div>
+    </>
+  );
 }
--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -9,6 +9,7 @@ import {
   TimelineTitle,
 } from "flowbite-react";
 import styles from "./Work.module.css";
+import { HiBriefcase, HiAcademicCap} from "react-icons/hi";
 
 export default function Work() {
   const workExperience = [
@@ -16,15 +17,29 @@ export default function Work() {
       date: "June 2024 - Present",
       title: "Full-Stack Software Engineer",
       company: "Deloitte",
+      icon: HiBriefcase,
       description: [
         "Developed and maintained front-end features for a high-traffic government web application using React, JavaScript, and TypeScript",
-        "Played a key role in the migration from JavaScript to TypeScript, becoming a go-to resource for best practices and onboarding teammates",
+        "Led efforts in migrating the codebase from JavaScript to TypeScript, becoming a key resource for best practices and supporting teammates during the transition.",
       ],
+    },
+    {
+      date: "April 2024",
+      icon: HiAcademicCap,
+      title: "AWS Certified Cloud Practitioner",
+      company: "Amazon Web Services",
+    },
+    {
+      date: "April 2024",
+      icon: HiAcademicCap,
+      title: "Java Certified Foundations Associate",
+      company: "Oracle",
     },
     {
       date: "March 2024 - Present",
       title: "Java Developer Apprentice",
       company: "Skillstorm",
+      icon: HiBriefcase,
       description: [
         "Built full-stack web applications using Java, Spring Boot, React, TypeScript, and PostgreSQL, delivering scalable features across both front-end and back-end systems",
         "Deployed microservices using Docker, Kubernetes, and AWS, and strengthened cloud and Java expertise by earning AWS Cloud Practitioner and Java Foundations certifications",
@@ -34,6 +49,7 @@ export default function Work() {
       date: "July 2023 - September 2023",
       title: "Web Developer",
       company: "Freelance",
+      icon: HiBriefcase,
       description: [
         "Independently led the end-to-end design and development of a 7-page web application, completing the project two weeks ahead of schedule and exceeding client expectations",
         "Collaborated closely with the client to define scope and requirements, applying strong project planning, time management, and communication skills to ensure a smooth delivery process",
@@ -43,6 +59,7 @@ export default function Work() {
       date: "March 2023 - June 2023",
       title: "Software Engineering Fellow",
       company: "General Assembly",
+      icon: HiBriefcase,
       description: [
         "Completed immersive, hands-on training in software engineering with a focus on JavaScript and full-stack web development through real-world, project-based learning",
         "Gained practical experience with Git, GitHub workflow, and Agile methodologies, strengthening both technical and collaboration skills in a fast-paced team environment",
@@ -50,27 +67,30 @@ export default function Work() {
     },
   ];
   return (
-    <div className={`container ${styles.workContainer}`}>
-      <Timeline>
-        {workExperience.map((experience) => (
-          <TimelineItem key={experience.company}>
-            <TimelinePoint />
-            <TimelineContent className={styles.timelineContent}>
-              <TimelineTime>{experience.date}</TimelineTime>
-              <br />
-              <TimelineTime>{experience.company}</TimelineTime>
-              <TimelineTitle>{experience.title}</TimelineTitle>
-              <TimelineBody>
-                <ul>
-                  {experience.description.map((bullet, index) => (
-                    <li key={index}>{bullet}</li>
-                  ))}
-                </ul>
-              </TimelineBody>
-            </TimelineContent>
-          </TimelineItem>
-        ))}
-      </Timeline>
-    </div>
+    <>
+      <h1 className={styles.title}>Education - Experience</h1>
+      <div className={`container ${styles.workContainer}`}>
+        <Timeline>
+          {workExperience.map((experience) => (
+            <TimelineItem key={experience.company}>
+              <TimelinePoint icon={experience.icon} />
+              <TimelineContent className={styles.timelineContent}>
+                <TimelineTime>{experience.date}</TimelineTime>
+                <br />
+                <TimelineTime>{experience.company}</TimelineTime>
+                <TimelineTitle>{experience.title}</TimelineTitle>
+                <TimelineBody>
+                  <ul>
+                    {experience.description && experience.description.map((bullet, index) => (
+                      <li key={index}>{bullet}</li>
+                    ))}
+                  </ul>
+                </TimelineBody>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
+        </Timeline>
+      </div>
+    </>
   );
 }
